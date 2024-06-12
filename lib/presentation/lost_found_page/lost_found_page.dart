c00cf3be861df6645340cd66dc0bacc5a792fba5
sandrodmuhammad <9c.sandro@gmail.com>
2024-06-12 09:54:41 +0700

    lost and found, profile

--- lib/presentation/lost_found_page/lost_found_page.dart
+++ lib/presentation/lost_found_page/lost_found_page.dart
@@ -1,12 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class LostFoundPage extends StatelessWidget {
+class LostFoundPage extends StatefulWidget {
+  @override
+  _LostFoundPageState createState() => _LostFoundPageState();
+}
+
+class _LostFoundPageState extends State<LostFoundPage> {
+  List<String> items = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchItems();
+  }
+
+  Future<void> fetchItems() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (mounted) {
+        setState(() {
+          items = data.map((item) => item['title'] as String).toList();
+          isLoading = false;
+        });
+      }
+    } else {
+      throw Exception('Failed to load items');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Lost & Found Page'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Lost & Found',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: isLoading
+                ? Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 16),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 8,
+                                  offset: Offset(0, 4),
+                                ),
+                              ],
+                            ),
+                            child: ListTile(
+                              contentPadding: EdgeInsets.all(16),
+                              title: Text(
+                                items[index],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontWeight: FontWeight.normal),
+                              ),
+                              onTap: () {
+                                // Handle item click
+                              },
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+          ),
+        ],
       ),
+      floatingActionButton: SizedBox(
+        width: 60, 
+        height: 60,
+        child: FloatingActionButton(
+          onPressed: () {
+            // Handle FAB click
+          },
+          child: Icon(Icons.add, size: 36, color: Colors.white),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          backgroundColor: Color(0xFF27ae60),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
--- lib/presentation/profile_page.dart/profile_page.dart
+++ lib/presentation/profile_page.dart/profile_page.dart
@@ -1,11 +1,153 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String username = "John Doe";
+  String phone = "01234567890";
+  String appVersion = "2.0.0";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    // Simulate fetching user data and app version
+    await Future.delayed(Duration(seconds: 2));
+    if (mounted) {
+      setState(() {
+        username = "User"; // Replace with actual user data fetching logic
+        phone = "081234567890"; // Replace with actual user data fetching logic
+        appVersion = "2.0.0"; // Replace with actual app version fetching logic
+      });
+    }
+  }
+
+  void logout() {
+    // Implement logout functionality here
+    print("User logged out");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Profile Page'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                  'https://www.gravatar.com/avatar/placeholder', // Replace with actual image URL
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                username,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey[900],
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                phone,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.blueGrey[700],
+                ),
+              ),
+              SizedBox(height: 24),
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  double containerWidth = constraints.maxWidth;
+
+                  return Column(
+                    children: [
+                      Container(
+                        width: containerWidth,
+                        padding: const EdgeInsets.all(24.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 4,
+                              spreadRadius: 2,
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/image/logo-kontena.png',
+                              height: 45,
+                            ),
+                            SizedBox(width: 16),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Kontena - House Keeping',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blueGrey[800],
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  'V $appVersion',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.blueGrey[600],
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 100),
+                      SizedBox(
+                        width: containerWidth,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          onPressed: () {
+                            print("log out");
+                          },
+                          child: Text(
+                            'Log Out',
+                            style: TextStyle(
+                              fontFamily: 'OpenSans',
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
